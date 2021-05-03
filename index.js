36f74111a3165e56a64c51e963877f5cb50962b2
Emem Isaac <me@ememisaac.com>
2021-05-03 15:10:16 +0100

    setting and reading state

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 class Hello extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state =  { time: new Date().toLocaleString()}
+    }
+
     render() {
         return React.createElement('h1', {
             className: 'orange'
-        }, `Hi there! @ ${this.props.time}`);
+        }, `Hi there! @ ${this.state.time}`);
     }
 } 
 ReactDOM.render(React.createElement(Hello,{ time: new Date()}, null), document.getElementById("app"));
