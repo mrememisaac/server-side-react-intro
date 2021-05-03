2e60cbc50438b012e60904cbd87adc3cac332b52
Emem Isaac <me@ememisaac.com>
2021-05-03 15:17:44 +0100

    adds a tick funtion which overrides the time every second, invokes the interval on component mount

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ class Hello extends React.Component {
         this.state =  { time: new Date().toLocaleString()}
     }
 
+    tick(){
+        this.setState(() => {
+            return ({
+                time: new Date().toLocaleString()
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentDidUnMount() {
+        this.clearInterval(this.interval);
+    }
+
     render() {
         return React.createElement('h1', {
             className: 'orange'
-        }, `Hi there! @ ${this.state.time}`);
+        }, `${this.state.time}`);
     }
 } 
 ReactDOM.render(React.createElement(Hello,{ time: new Date()}, null), document.getElementById("app"));
