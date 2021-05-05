fc5503b4c5a14c97cc9a34053eed1b0515367c72
Emem Isaac <me@ememisaac.com>
2021-05-05 12:30:42 +0100

    Replace the clock code with the digital clock widget

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
+import DigitalClock from '../src/DigitalClock/DigitalClock'
 
 class Index extends React.Component {
 
     constructor(props){
         super(props);
-        this.state =  { time: new Date().toLocaleString()}
-    }
-
-    tick(){
-        this.setState(() => {
-            return ({
-                time: new Date().toLocaleString()
-            });
-        });
-    }
-
-    componentDidMount(){
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    componentDidUnMount() {
-        this.clearInterval(this.interval);
-    }
+        this.state =  { time: new Date() }
+    }    
 
     render() {
-        return <h1>{this.state.time}</h1>
+        return <DigitalClock time={this.state.time}/>
     }
 } 
 export default Index;
